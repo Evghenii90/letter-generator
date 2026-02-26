e6dd365490bf2d3bbb2866c4a785aaf7b2a2a2d2
Evghenii90 <litvinenkoevgeniy3359@gmail.com>
2026-02-26 21:00:55 +0200

    init

---
+++ eslint.config.js
@@ -0,0 +1,67 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import {defineConfig, globalIgnores} from 'eslint/config'
+import unicorn from 'eslint-plugin-unicorn'
+
+import prettierConfig from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+export default defineConfig([
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        plugins: {
+            unicorn,
+            'prettier': pluginPrettier
+        },
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+            prettierConfig
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            'unicorn/better-regex': 'error',
+            'unicorn/error-message': 'error',
+            'unicorn/consistent-function-scoping': 'error',
+            'unicorn/filename-case': ['error', {cases: {camelCase: true, pascalCase: true,},},],
+            'unicorn/import-style': 'error',
+            'unicorn/no-array-callback-reference': 'error',
+            'unicorn/no-console-spaces': 'error',
+            'unicorn/no-null': 'off',
+            'unicorn/no-empty': "error",
+            'unicorn/no-process-exit': 'error',
+            'unicorn/no-this-assignment': 'error',
+            'unicorn/no-unused-properties': 'warn',
+            'unicorn/no-useless-undefined': 'error',
+            'unicorn/prefer-optional-catch-binding': 'error',
+
+            'prettier/prettier': [
+                'error',
+                {
+                    semi: true,
+                    singleQuote: true,
+                    tabWidth: 2,
+                    printWidth: 100,
+                    trailingComma: 'es5',
+                    bracketSpacing: true,
+                    arrowParens: 'always',
+                    endOfLine: 'lf',
+                },
+            ],
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+])
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import {useState} from "react";// Unicorn найдет это
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </>
+  )
+}
+
+export default App
